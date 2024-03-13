0456ff6440652f9ec30eab52b3fbea3a6d85d102
agathaa <aamoako1120@gmail.com>
2024-03-12 21:34:41 -0400

    updated hero section

---
+++ acs-website/src/app/compontents/HeroSection.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const HeroSection = () => {
+  return (
+    <section className="lg:py-16">
+      <div className="grid grid-rows-1 lg:grid-rows-12">
+        <h1>American Cooperative School</h1>
+        <p>
+          The need for educating the youth is on the rise. This has encouraged
+          many private individuals to establish more private schools since the
+          government of Ghana is unable to accommodate all the children in the
+          public schools. Many of these private schools provide a better
+          learning environment and quality education than the public schools at
+          the primary and Junior High Schools level. American Cooperative School
+          exemplifies one of the modern schools that aim at providing quality
+          basic education for those students who are in its Preschool, Primary
+          and Junior High School grades level.
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default HeroSection;
--- acs-website/src/app/page.tsx
+++ acs-website/src/app/page.tsx
@@ -1,9 +1,11 @@
+import HeroSection from "./compontents/HeroSection";
+
 
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      
+     <HeroSection />
     </main>
   );
 }
