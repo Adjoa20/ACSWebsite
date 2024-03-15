c1c2bab72a66aa9270ce0747d54866e9b30faf1b
agathaa <aamoako1120@gmail.com>
2024-03-14 20:12:58 -0400

    updated navbar in large and mobile screen

--- acs-website/src/app/compontents/NavLinks.tsx
+++ acs-website/src/app/compontents/NavLinks.tsx
@@ -6,61 +6,60 @@ import { links } from './Links'
 import { ChevronDownIcon } from '@heroicons/react/24/solid'
 
 const NavLinks = () => {
-    const [openMenu, setOpenMenu] = useState<string | null>(null)
-
-    const handleMenuToggle = (name: string) => {
-        setOpenMenu(openMenu !== name ? name : null)
-    }
-
+   const [heading, setHeading] = useState('')
     return (
         <>
             {links.map((link) => (
                 <div key={link.name}>
                     <div className='px-3 text-left md:cursor-pointer group'>
-                        <h1
-                            className='py-7 flex justify-between items-center md:pr-0 pr-5'
-                            onClick={() => handleMenuToggle(link.name)}
-                        >
-                            {link.name}
-                            <span className='text-xl md:mt-1 md:hidden inline'>
-                                <ChevronDownIcon className='h-5 w-5 md:h-3 md:w-3' />
-                            </span>
-                            <span className={`text-xl md:mt-1 md:ml-2 md:block hidden ${openMenu === link.name ? 'transform rotate-180 mt-2' : ''}`}>
-                                <ChevronDownIcon className='h-5 w-5 md:h-3 md:w-3' />
-                            </span>
+                        <h1 className='py-7 flex justify-between items-center md:pr-0 pr-5' onClick={() => heading !== link.name ? setHeading(link.name) : setHeading('')}>{link.name}
+                        <span className='text-xl md:mt-1 md:hidden inline'>
+                           <ChevronDownIcon className='h-5 w-5 md:h-3 md:w-3'/> 
+                        </span>
+                        <span className='text-xl md:mt-1 md:ml-2 md:block hidden group-hover:rotate-180 group-hover:mt-2'>
+                           <ChevronDownIcon className='h-5 w-5 md:h-3 md:w-3'/> 
+                        </span>
                         </h1>
-                        {link.submenu && openMenu === link.name && (
-                            <div className='absolute top-20 hidden md:block group-hover:block'>
-                                <div className='py-3'>
-                                    <div className='w-4 h-4 left-3 absolute mt-1 bg-black rotate-45' />
-                                </div>
-                                <div className='bg-black p-5 grid grid-cols-1 gap-1'>
-                                    {link.sublinks.map((mysubLinks) => (
-                                        <div key={mysubLinks.sublink[0].name}>
-                                            <h1 className='text-sm font-light'>
-                                                {mysubLinks.sublink.map((sLink) => (
-                                                    <li key={sLink.name} className='text-sm text-gray-600 my-2.5'>
-                                                        <Link href={sLink.link} className='hover:text-slate-500'>{sLink.name}</Link>
-                                                    </li>
-                                                ))}
-                                            </h1>
+                        {link.submenu && (
+                            <div>
+                                <div className='absolute top-20 hidden group-hover:md:block hover:md:block'>
+                                    <div className='py-3'>
+                                        <div className='w-4 h-4 left-3 absolute mt-1 bg-black rotate-45'>
+
                                         </div>
-                                    ))}
+                                    </div>
+                                    <div className='bg-black p-5 grid grid-cols-1 gap-1'>
+                                        {link.sublinks.map((mysubLinks) => (
+                                            <div key={mysubLinks.sublink[0].name}>
+                                                <h1 className='text-sm font-light'>
+                                                    {mysubLinks.sublink.map((sLink) => (
+                                                        <li key={sLink.name} className='text-sm text-gray-600 my-2.5'>
+                                                            <Link href={sLink.link} className='hover:text-slate-500'>{sLink.name}</Link>
+                                                        </li>
+                                                    ))}
+                                                </h1>
+                                            </div>
+                                        ))}
+                                    </div>
                                 </div>
                             </div>
                         )}
                     </div>
-                    {/* FOR MOBILE USE */}
-                    <div className={`md:hidden ${openMenu === link.name ? 'block' : 'hidden'}`}>
-                        {link.sublinks.map((sLinks) => (
-                            <div key={sLinks.sublink[0].name}>
-                                {sLinks.sublink.map((sLink) => (
-                                    <li className='py-3 pl-14' key={sLink.name}>
-                                        <Link href={sLink.link} className='hover:text-slate-400'>{sLink.name}</Link>
-                                    </li>
-                                ))}
-                            </div>
-                        ))}
+                    {/* FOR MOBILE USE  */}
+                    <div className={`${heading === link.name ? 'md:hidden' : 'hidden'}`}>
+                        {
+                            link.sublinks.map((sLinks)=>(
+                                <div>
+                                    <div>
+                                        {sLinks.sublink.map((sLink) => (
+                                            <li className='py-3 pl-14'>
+                                                <Link href={sLink.link} className='hover:text-slate-400'>{sLink.name}</Link>
+                                            </li>
+                                        ))}
+                                    </div>
+                                </div>
+                            ))
+                        }
                     </div>
                 </div>
             ))}
@@ -69,5 +68,3 @@ const NavLinks = () => {
 }
 
 export default NavLinks
-
-
